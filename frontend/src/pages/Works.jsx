8392dae1368e42f6b90028a6c71744d752137eed
Hari-nandana <harinandana2004@gmail.com>
2025-09-13 17:59:15 +0530

    header-not responsive

---
+++ frontend/src/pages/Works.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Works = () => {
+  return (
+    <div style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}>
+      <Header />
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <p>Haiii..Helloo</p>
+      </div>
+    </div>
+  );
+};
+
+export default Works;
