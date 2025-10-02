2fc8fc8215a7ca484bc3e8e6e68a4f3632d1d76c
Hari-nandana <harinandana2004@gmail.com>
2025-10-02 16:52:32 +0530

    why Join Us-completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Choose from "./pages/Choose";
+import JoinUs from "./pages/JoinUs";
 
 function App() {
   return (
     <div>
-      <Choose/>
+      <Header />
+      <Choose />
+      
     </div>
   );
 }
