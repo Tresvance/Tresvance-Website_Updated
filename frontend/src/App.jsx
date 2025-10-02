1b46d3ff3dbd30d364e863a4cf07495958514a42
Hari-nandana <harinandana2004@gmail.com>
2025-10-02 17:12:02 +0530

    Save current work before switching branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import Choose from "./pages/Choose";
 import Footer from "./components/Footer";
+import JoinUs from "./pages/JoinUs";
 
 
 function App() {
   return (
     <div id="top">
       {/* Your page content */}
+      
       <Choose/>
-      <Footer />
+      <Footer/>
+ 
     </div>
   );
 }
