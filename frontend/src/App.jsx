1f95c9445eaabf8e1b06e2457ad9f94b510bbc1c
Hari-nandana <harinandana2004@gmail.com>
2025-10-10 00:11:46 +0530

    Header updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Choose from "./pages/Choose";
 import JoinUs from "./pages/JoinUs";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Choose />
-      
-    </div>
+      <Routes>
+        <Route path="/" element={<Choose />} />
+        <Route path="/choose-us" element={<Choose />}/>
+        <Route path="/join-us" element={<JoinUs />} />
+      </Routes>
+    </Router>
   );
 }
 
