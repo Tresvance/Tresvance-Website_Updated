43e1b78029cf24ac1c126f2a9c2174e42a3de3ee
unknown <ahamedhishamtp@gmail.com>
2025-10-10 21:00:26 +0530

    feat(frontend): updated responsive footer and added About Us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Choose from "./pages/Choose";
+import AboutUs from "./pages/AboutUs";
 import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div id="top">
-      {/* Your page content */}
+      {/* <AboutUs /> */}
       <Footer />
     </div>
   );
